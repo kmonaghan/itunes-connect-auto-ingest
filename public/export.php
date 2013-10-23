0bc3c50545a04202d35b56a7f46790823f775b54
Karl Monaghan <karl.t.monaghan@gmail.com>
2013-10-23 15:23:40 +0100

    Handle exatra category field added in the last 2 weeks

--- public/export.php
+++ public/export.php
@@ -1,19 +1,28 @@
 <?php
 include 'boot.php';
 
-$query = "SELECT * FROM daily_raw ORDER BY id";
+$query = "SELECT * FROM daily_raw where begin_date >= '2012-11-20' and begin_date < '2012-12-03' ORDER BY id";
 
 $sth_total = $dbh->prepare($query);
 
 $sth_total->execute();
 
-$fp = fopen('file.csv', 'w');
+$fp = fopen('export.csv', 'w');
+
+$header = array('Provider','Provider Country','SKU','Developer','Title','Version','Product', 'Type', 'Identifier', 'Units', 'Developer Proceeds', 'Begin Date', 'End Date', 'Customer Currency', 'Country Code', 'Currency of Proceeds', 'Apple Identifier', 'Customer Price', 'Promo Code', 'Parent Identifier', 'Subscription', 'Period', 'Category');
+
+fputcsv($fp, $header, "\t");
 
 while ($row = $sth_total->fetch(PDO::FETCH_ASSOC))
 {
 
 $row['begin_date'] = date('m/d/Y', strtotime($row['begin_date']));
 $row['end_date'] = date('m/d/Y', strtotime($row['end_date']));
+$row['product_type_identifier'] = (int)$row['product_type_identifier'];
+$row['units'] = (int)$row['units'];
+$row['developer_proceeds'] = (int)$row['developer_proceeds'];
+$row['customer_price'] = (int)$row['customer_price'];
+
 unset($row['id']);
 	fputcsv($fp, $row, "\t");
 }
--- schema.sql
+++ schema.sql
@@ -1,3 +1,30 @@
+# ************************************************************
+# Sequel Pro SQL dump
+# Version 4096
+#
+# http://www.sequelpro.com/
+# http://code.google.com/p/sequel-pro/
+#
+# Host: localhost (MySQL 5.5.28)
+# Database: ios
+# Generation Time: 2013-10-23 14:18:37 +0000
+# ************************************************************
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+# Dump of table daily_raw
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `daily_raw`;
+
 CREATE TABLE `daily_raw` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `provider` char(5) DEFAULT 'APPLE',
@@ -16,12 +43,23 @@ CREATE TABLE `daily_raw` (
   `currency_proceeds` char(3) DEFAULT NULL,
   `apple_identifier` decimal(18,0) DEFAULT NULL,
   `customer_price` decimal(18,2) DEFAULT NULL,
-  `promo_code` varchar(10) DEFAULT NULL,
-  `parent_identifier` varchar(100) DEFAULT NULL,
-  `subscription` varchar(10) DEFAULT NULL,
-  `period` varchar(30) DEFAULT NULL,
+  `promo_code` varchar(10) DEFAULT ' ',
+  `parent_identifier` varchar(100) DEFAULT ' ',
+  `subscription` varchar(10) DEFAULT ' ',
+  `period` varchar(30) DEFAULT ' ',
+  `category` varchar(30) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `apple_identifier` (`apple_identifier`),
   KEY `begin_date` (`begin_date`),
   KEY `product_type_identifier` (`product_type_identifier`)
-) ENGINE=MyISAM AUTO_INCREMENT=305 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+
+
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
